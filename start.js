61b54c206e6cdd9c6ae575cfffd8be1ff4b54cdb
MrBobertus <mr.bobertus.dev@gmail.com>
2025-08-04 09:24:34 +0200

    Initial Node-RED setup for Hosting

--- start.js
+++ start.js
@@ -1,21 +1,35 @@
-// start.js
-const RED = require("node-red");
-const http = require("http");
+const http = require('http');
+const express = require('express');
+const RED = require('node-red');
 
-const server = http.createServer(function (req, res) {
-  res.writeHead(404);
-  res.end();
-});
+const app = express();
+const server = http.createServer(app);
 
 const settings = {
-  httpAdminRoot: "/",
-  httpNodeRoot: "/api",
-  userDir: "./",
+  httpAdminRoot: '/',
+  httpNodeRoot: '/api',
+  userDir: './',
   functionGlobalContext: {},
-  flowFile: "flows.json",
-  settingsFile: __dirname + "/settings.js",
+  flowFile: 'flows.json',
   uiPort: process.env.PORT || 1880,
+  credentialSecret: process.env.CREDENTIAL_SECRET || 'my-node-red-secret',
+  adminAuth: {
+    type: "credentials",
+    users: [{
+      username: process.env.NODE_RED_USERNAME,
+      password: process.env.NODE_RED_PASSWORD_HASH,
+      permissions: "*"
+    }]
+  }
 };
 
 RED.init(server, settings);
+
+app.use(settings.httpAdminRoot, RED.httpAdmin);
+app.use(settings.httpNodeRoot, RED.httpNode);
+
+server.listen(settings.uiPort, () => {
+  console.log(`Node-RED is running on port ${settings.uiPort}`);
+});
+
 RED.start();
